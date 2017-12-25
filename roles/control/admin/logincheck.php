c163c8130e58d4ea8c51bab9a3607eaf0d67c557
Niaz <trialnly@gmail.com>
2017-12-25 16:47:07 +0600

    little change

--- roles/control/admin/logincheck.php
+++ roles/control/admin/logincheck.php
@@ -2,8 +2,8 @@
     session_start();
     if(!(isset($_SESSION['admin_username'])))
     {
-        echo "<h1 align='center'>Restricted Access</h1><hr>";
-        echo "<h1 align='center'Redirecting to login page</h1>";
+        echo "<h1 align='center'>Restricted Access</h1>";
+        echo "<h3 align='center'>Redirecting to login page</h4><hr>";
         header( "refresh:5; url=http://localhost/DataShelf/roles/presentation/admin/admin_login.php" ); 
         die();
     }
--- roles/presentation/admin/admin_history.php
+++ roles/presentation/admin/admin_history.php
@@ -1,3 +1,4 @@
+<?php require_once "../../control/admin/logincheck.php" ?>
 <!DOCTYPE html>
 <html>
   <head>
