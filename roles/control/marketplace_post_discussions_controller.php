e9570c4ff0a53f39839a8b2521a215fb6f8113ed
Niaz <trialnly@gmail.com>
2017-12-28 00:00:03 +0600

    bugfix

--- roles/control/marketplace_post_discussions_controller.php
+++ roles/control/marketplace_post_discussions_controller.php
@@ -12,7 +12,7 @@ if(isset($_GET['id']))
     $pz = $all_info['price'];
     $price = $pz == 0 ? "FREE": '$ '.$pz;
     $download = $all_info['downloads'];
-    $downloads = $download==0?"None has purchased this datasest yet. Don't miss the chance to be the first one !":$download+ "downloads";
+    $downloads = $download==0?"None has purchased this datasest yet. Don't miss the chance to be the first one !":$download. " downloads";
     $date = get_upload_date_by_id($mds_id);
     $upload_date = ($date == 0 ? "Today" : $date." days ago");
  
